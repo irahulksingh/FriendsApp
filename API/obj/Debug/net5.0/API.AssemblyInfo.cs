4af49b24aef5aec2823e89f408d9dc5762d9ea15
meRahulSingh <mr.rahulsingh@hotmail.com>
2021-01-21 03:08:20 +0100

    Added DTO

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1947fe929c18b4bc310cb1fbe1e3b7d41fc871e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3000fc71cc8dcad3eee4e12e5b1ba030ec406f39")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
